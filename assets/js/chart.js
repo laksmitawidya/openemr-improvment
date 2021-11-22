b1809ab22ff91730f7e30ae55caf44f21dd2bc30
Vian Handika <32483035+vianhandika@users.noreply.github.com>
2021-11-22 23:16:29 +0700

    Adding index route by role + index accountant

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -19,17 +19,94 @@ $(document).ready(function() {
 		}]
 	};
 
-	var ctx = document.getElementById('bargraph').getContext('2d');
-	window.myBar = new Chart(ctx, {
-		type: 'bar',
-		data: barChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
+	var ctx = document.getElementById('bargraph');
+	if(ctx){
+		ctx.getContext('2d');
+		window.myBar = new Chart(ctx, {
+			type: 'bar',
+			data: barChartData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				}
 			}
-		}
-	});
+		});
+	}
+
+
+	// Bar Chart Cash Accounting
+
+	var cashflowData = {
+		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+		datasets: [{
+			label: 'Income',
+			backgroundColor: 'rgba(0, 255, 0, 0.5)',
+			borderColor: 'rgba(0, 255, 0, 1)',
+			borderWidth: 1,
+			data: [35000, 59000, 80000, 81000, 56000, 55000, 40000]
+		}, {
+			label: 'Expense',
+			backgroundColor: 'rgba(255, 0, 0, 0.5)',
+			borderColor: 'rgba(255, 0, 0, 1)',
+			borderWidth: 1,
+			data: [28000, 48000, 40000, 19000, 86000, 27000, 90000]
+		}]
+	};
+
+	var ctx_cashflow = document.getElementById('bargraph-income-expense');
+	if(ctx_cashflow){
+		ctx_cashflow.getContext('2d');
+		window.myBar = new Chart(ctx_cashflow, {
+			type: 'bar',
+			data: cashflowData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				},
+			}
+		});
+	}
+
+	//Doughnut expense pending Chart
+	var expenseStatusData = {
+		labels: [
+			'Approved',
+			'Pending',
+
+		],
+		datasets: [{
+			label: 'Status',
+			data: [1, 1],
+			backgroundColor: [
+				'rgba(0, 255, 0, 0.5)',
+				'rgba(255, 0, 0, 0.5)',
+			],
+			borderColor:[
+				'rgba(0, 255, 0, 0.5)',
+				'rgba(255, 0, 0, 0.5)',
+			],
+			hoverOffset: 4
+		}]
+	}; 
+
+	var ctx_expenseStatus = document.getElementById('doughnut-expense-status');
+	if(ctx_expenseStatus){
+		ctx_expenseStatus.getContext('2d');
+		window.myBar = new Chart(ctx_expenseStatus, {
+			type: 'doughnut',
+			data: expenseStatusData,
+			options: {
+				responsive: true,
+				// legend: {
+				// 	display: false,
+				// },
+			}
+		});
+	}
+
+	
 
 	// Line Chart
 
@@ -40,28 +117,64 @@ $(document).ready(function() {
 			backgroundColor: "rgba(0, 158, 251, 0.5)",
 			data: [100, 70, 20, 100, 120, 50, 70, 50, 50, 100, 50, 90]
 		}, {
-		label: "My Second dataset",
-		backgroundColor: "rgba(255, 188, 53, 0.5)",
-		fill: true,
-		data: [28, 48, 40, 19, 86, 27, 20, 90, 50, 20, 90, 20]
+			label: "My Second dataset",
+			backgroundColor: "rgba(255, 188, 53, 0.5)",
+			fill: true,
+			data: [28, 48, 40, 19, 86, 27, 20, 90, 50, 20, 90, 20]
 		}]
 	};
 	
-	var linectx = document.getElementById('linegraph').getContext('2d');
-	window.myLine = new Chart(linectx, {
-		type: 'line',
-		data: lineChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
+	var linectx = document.getElementById('linegraph');
+	if(linectx){
+		linectx.getContext('2d');
+		window.myLine = new Chart(linectx, {
+			type: 'line',
+			data: lineChartData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				}
 			}
-		}
-	});
+		});
+	}
+
+
+	// Line Chart INCOME OUTCOME
+
+	// var lineChartData = {
+	// 	labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+	// 	datasets: [{
+	// 		label: "Income",
+	// 		backgroundColor: "red",
+	// 		data: [100000, 70000, 20000, 100000, 120000, 50000, 70000, 50000, 50000, 100000, 50000, 90000]
+	// 	}, {
+	// 		label: "My Second dataset",
+	// 		backgroundColor: "rgba(255, 188, 53, 0.5)",
+	// 		fill: true,
+	// 		data: [28, 48, 40, 19, 86, 27, 20, 90, 50, 20, 90, 20]
+	// 	}]
+	// };
+	
+	// var linectx = document.getElementById('linegraph').getContext('2d');
+	// window.myLine = new Chart(linectx, {
+	// 	type: 'line',
+	// 	data: lineChartData,
+	// 	options: {
+	// 		responsive: true,
+	// 		legend: {
+	// 			display: false,
+	// 		},
+	// 		tooltips: {
+	// 			mode: 'index',
+	// 			intersect: false,
+	// 		}
+	// 	}
+	// });
 	
 	// Bar Chart 2
 	
